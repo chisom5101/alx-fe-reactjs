e17f5320eff64cac46ba8380d69e08d0a4abb076
Chisom5101 <chisomisiguzor94@gmail.com>
2025-07-11 13:32:52 +0100

    react

--- alx-react-app/src/App.jsx
+++ alx-react-app/src/App.jsx
@@ -3,6 +3,9 @@ import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
+import Header from './Header'
+import MainContent from './MainContent'
+import Footer from './Footer'
 
 function App(<WelcomeMessage />) {
   const [count, setCount] = useState(0)
