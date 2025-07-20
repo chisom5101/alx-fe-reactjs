553a8d8c3ba2cfefa1487a1a1b8f71f618600e6b
Chisom5101 <chisomisiguzor94@gmail.com>
2025-07-20 21:19:04 +0100

    new react app

--- alx-react-app-new/src/App.jsx
+++ alx-react-app-new/src/App.jsx
@@ -1,8 +1,19 @@
 import React from "react";
 import UserProfile from "./components/UserProfile";
+import UserContext from './UserContext';
+import Counter from './components/Counter';
 
 function App() {
   return (
+    <UserContext.Provider value={userdata}>
+      <div className="App">
+        <Header />
+        <MainContent />
+        <Counter />
+        <ProfilePage />
+        <Footer />
+
+
     <div>
       <UserProfile name="Alice" age="25" bio="Loves hiking and photography" />
     </div>
